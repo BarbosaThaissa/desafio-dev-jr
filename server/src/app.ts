6cb82778b8f175ef9562da092234a5983c92d6a7
Barbosa Thaíssa <thathamaria03@gmail.com>
2023-05-28 20:09:32 -0300

    fix: add cors

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,8 +1,10 @@
 import express from "express";
+import cors from "cors";
 import tasksRouter from "./routes/tasks";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use("/tasks", tasksRouter);
 
